a71996fd2a604858e5c75ec4e89541e52ed36cd9
Megan <meganbickel@gmail.com>
2022-02-02 20:45:52 -0600

    commit on main

--- JS/sampleJS.js
+++ JS/sampleJS.js
@@ -1,3 +1,17 @@
 function howdy(FirstName){
     alert("Howdy " + FirstName);
 }
+function conditional(){
+    alert("Use Inspect to see the console and inspect the code.");
+    var currentHour = new Date().getHours();
+    if (currentHour <10) {
+        alert("Good morning!");
+    } else if (currentHour < 18) {
+        alert("Good day!");
+    } else {
+        alert ("Good evening!");
+    } 
+}
+for (i = 0; i < 5; i++) {
+    console.log("The number is " + i);
+}
